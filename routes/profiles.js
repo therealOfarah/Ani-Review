8cb1cc7f139e49123b62e8ebe1fffc8a0dfdea7a
therealofarah <omaarfaarah2019@gmail.com>
2022-06-30 20:20:37 -0400

    add delete button

--- routes/profiles.js
+++ routes/profiles.js
@@ -8,7 +8,7 @@ router.get('/:id', profileCrtl.show)
 router.get('/:id/edit', isLoggedIn, profileCrtl.edit)
 router.put('/:id', isLoggedIn, profileCrtl.update)
 // router.post('/addAnime', profileCrtl.addAnime)
-router.post("/:id/reviews",profileCrtl.create)
+router.post("/:id/reviews", profileCrtl.create)
 // router.post("/:id/reviews",profileCrtl.createReview)
 router.delete('/:id/reviews/:reviewId', profileCrtl.delete)
 export {
